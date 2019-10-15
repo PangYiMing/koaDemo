320655c9a3cffdcfde6c1dd882544cc23980786a
庞熠明 <995637308@qq.com>
2019-10-15 17:29:59 +0800

    修改

--- demo24.js
+++ demo24.js
@@ -1,55 +1,73 @@
-// re_require.
+const fs = require('fs')
+const mypath = require('path')
+const fetch = require("node-fetch")
 
-const fs = require('fs');
-const fetch = require('node-fetch')
 const relativePath = 'HostJson'
-const filePath = __dirname + '/' + relativePath + '.js'
-const GOBACKEND = 'localhost:9090'
-console.log('filePath:', filePath, 'GOBACKEND:', GOBACKEND)
-
-async function testReQuire(path) {
-  // 从go拿到配置创建域名配置文件，如果有就引用，尽量确保成功,失败了也不要更改内容
-  var hostJson = require('./' + relativePath + '.js')
-  console.log('hostJson1', hostJson)
-
-  // 需要从go拿到配置创建域名配置文件 render 重启调用。每次接口触发调用
-  try {
-    const res = await fetch(`http://${GOBACKEND}/__domain/getHostConfig`, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      dataType: 'json',
-      method: 'post',
-      body: JSON.stringify({
-        token: '216e0c7a7e3dd426f118c74e8ddfd823'
-      })
-    })
-    const { code, msg, data } = await res.json()
-
-    if (code === '200') {
-      for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-          const element = data[key];
-          data[key] = JSON.parse(element)
-        }
-      }
-      console.log('data', data)
-      fs.writeFile(path, `module.exports = ${JSON.stringify(data)}`, function (err) {
-        if (err) {
-          throw err;
-        }
-        console.log('Saved.');
-        delete require.cache[path];
+const path = mypath.join(__dirname, relativePath + '.js')
+const GOBACKEND = '127.0.0.1:9090'
+function getHostConfig(app, httpListen) {
+  return new Promise(resolve => {
+    try {
+      // 从go拿到配置创建域名配置文件，如果有就引用，尽量确保成功,失败了也不要更改内容
+      var hostJson = {}
+      try {
         hostJson = require('./' + relativePath + '.js')
-        console.log('hostJson2', hostJson)
+      } catch (error) {
+        console.log('getHostConfig first error', error)
+      }
+      console.log('getHostConfig hostJson1', hostJson)
 
-      });
-    } else {
-      console.log(msg)
+
+      // 需要从go拿到配置创建域名配置文件 render 重启调用。每次接口触发调用
+      fetch(`http://${GOBACKEND}/__domain/getHostConfig`, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        dataType: 'json',
+        method: 'post',
+        body: JSON.stringify({
+          token: '216e0c7a7e3dd426f118c74e8ddfd823'
+        })
+      }).then((res) => {
+        res.json().then((result) => {
+          const { code, msg, data } = result
+          if (code === '200') {
+            for (const key in data) {
+              if (Object.prototype.hasOwnProperty.call(data, key)) {
+                const element = data[key]
+                data[key] = JSON.parse(element)
+              }
+            }
+            fs.writeFile(path, `module.exports = ${JSON.stringify(data)}`, function (err) {
+              if (err) {
+                throw err
+              }
+              delete require.cache[path]
+              hostJson = require('./' + relativePath + '.js')
+              console.log('getHostConfig json:', hostJson)
+              app && httpListen(app)
+              resolve(hostJson)
+            })
+          } else {
+            console.log('getHostConfig msg', msg, 'result', result)
+          }
+        }).catch((err) => {
+          app && httpListen(app)
+          console.log('getHostConfig catch', 1, ' hostJson:', hostJson, ' err:', err)
+          resolve(hostJson)
+        })
+      }).catch((err) => {
+        app && httpListen(app)
+        console.log('getHostConfig catch', 2, ' hostJson:', hostJson, ' err:', err)
+        resolve(hostJson)
+      })
+    } catch (err) {
+      app && httpListen(app)
+      console.log('getHostConfig catch', 3, ' hostJson:', hostJson, ' err:', err)
+      resolve(hostJson)
     }
-  } catch (err) {
-    console.log(err);
+  })
 
-  }
 }
-testReQuire(filePath, 1)
+module.exports = getHostConfig
+
--- j.js
+++ j.js
@@ -8,3 +8,4 @@ module.exports = {
   "beta": "[]",
   "pro": "[]"
 }
+var aaa = 'aas'
