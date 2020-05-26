563ccdf6d45a1a3be95b7f1e652956cc0b9a454a
Lyndon <995637308@qq.com>
2020-05-26 10:05:43 +0800

    update

--- demo06.js
+++ demo06.js
@@ -2,11 +2,20 @@
  * 静态目录
  */
 
-const Koa = require('koa');
-const path = require('path');
-const serve = require('koa-static');
+const Koa = require("koa");
+const path = require("path");
+const serve = require("koa-static");
 const app = new Koa();
-console.log(__dirname)
-const main = serve(path.join(__dirname+'/res'));
-app.use(main);
+console.log(__dirname);
+const main = serve(path.join(__dirname + "/res"));
+app
+  .use(async (ctx, next) => {
+    ctx.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "version",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+    });
+    await next();
+  })
+  .use(main);
 app.listen(3000);
